140fa7da1bf741d10d73b87a7d17ece7af39dd67
dominik <dominikheinisch2@gmail.com>
2019-10-10 20:33:05 +0200

    refactored simulations

---
+++ perceptron/consts.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+INPUT = np.asarray([[0, 0], [0, 1], [1, 0], [1, 1]])
+AND_OUTPUT = np.asarray([[0], [0], [0], [1]])
+OR_OUTPUT = np.asarray([[0], [1], [1], [1]])
+
+INPUT_BIPOLAR = np.asarray([[1, -1, -1], [1, -1, 1], [1, 1, -1], [1, 1, 1]])
+AND_OUTPUT_BIPOLAR = np.asarray([[-1], [-1], [-1], [1]])
+OR_OUTPUT_BIPOLAR = np.asarray([[-1], [1], [1], [1]])
