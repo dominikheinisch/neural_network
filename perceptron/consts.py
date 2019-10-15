bae3c450fdbf3e20f4ce7f6ffbe604eb245ce6b9
dominik <dominikheinisch2@gmail.com>
2019-10-16 01:35:51 +0200

    simulation refctor

--- perceptron/consts.py
+++ perceptron/consts.py
@@ -5,5 +5,6 @@ AND_OUTPUT = np.asarray([[0], [0], [0], [1]])
 OR_OUTPUT = np.asarray([[0], [1], [1], [1]])
 
 INPUT_BIPOLAR = np.asarray([[1, -1, -1], [1, -1, 1], [1, 1, -1], [1, 1, 1]])
+# INPUT_BIPOLAR = np.asarray([[0.99, -1, -1], [0.99, -1, 0.99], [0.99, 0.99, -1], [0.99, 0.99, 0.99]])
 AND_OUTPUT_BIPOLAR = np.asarray([[-1], [-1], [-1], [1]])
 OR_OUTPUT_BIPOLAR = np.asarray([[-1], [1], [1], [1]])
--- perceptron/simulation.py
+++ perceptron/simulation.py
@@ -6,15 +6,15 @@ from simulation_type import SimulationType
 def alphas_simulation(func, alphas, w_range, **kwargs):
     results = np.asarray([func(weights=np.random.uniform(-w_range, w_range, 3), alpha=a, **kwargs) for a in alphas])
     epochs = results[:, 1]
-    return epochs, alphas
     # print([(f'epochs={epochs}, alpha={a}') for (_, epochs), a in zip(results, alphas)])
+    return epochs, alphas
 
 
 def weight_ranges_simulation(func, alpha, w_ranges, **kwargs):
     results = np.asarray([func(weights=np.random.uniform(-w_r, w_r, 3), alpha=alpha, **kwargs) for w_r in w_ranges])
     epochs = results[:, 1]
+    # print([(f'epochs={epochs}, w_range=[-{w_r}:{w_r}]') for (_, epochs), w_r in zip(results, w_ranges)])
     return epochs, w_ranges
-    # print([(f'epochs={epochs}, weight_range=[-{w_r}:{w_r}') for (_, epochs), w_r in zip(results, w_ranges)])
 
 
 def run_many(name, params, n, simulation, **kwargs):
@@ -30,22 +30,26 @@ def run_many(name, params, n, simulation, **kwargs):
 
 def run_one(name, func, input, output, alpha, w_range, **kwargs):
     print(name)
-    weights = np.random.uniform(-w_range, w_range, 3)
+    weights = np.random.uniform(-w_range, w_range, input.shape[1])
     print(f'random weights: {weights}')
     result, epochs = func(input=input, output=output, alpha=alpha, weights=weights, **kwargs)
     print(f'result weights: {result}, epochs:{epochs}')
     print('input  output  calculated_output')
     for i in range(input.shape[0]):
         print(f'{input[i]}  {output[i]}  {input[i] @ result}')
+    # print('DDDDDD')
+    # print(f'{np.asarray([1, 0, 1])}  {output[i]}  {np.asarray([1, 0, -1]) @ result}')
+    # print(f'{np.asarray([1, 1, 0])}  {output[i]}  {np.asarray([1, -1, 0]) @ result}')
+
 
 
 if __name__== "__main__":
-    run_one(**SimulationType.PERCEPTRON_BIPOLAR_AND, alpha=0.01, w_range=0.2)
-    run_one(**SimulationType.PERCEPTRON_BIPOLAR_OR, alpha=0.01, w_range=0.2)
-    run_one(**SimulationType.PERCEPTRON_BINARY_AND, alpha=0.01, w_range=0.2)
-    run_one(**SimulationType.PERCEPTRON_BINARY_OR, alpha=0.01, w_range=0.2)
+    # run_one(**SimulationType.PERCEPTRON_BIPOLAR_AND, alpha=0.01, w_range=0.2)
+    # run_one(**SimulationType.PERCEPTRON_BIPOLAR_OR, alpha=0.01, w_range=0.2)
+    # run_one(**SimulationType.PERCEPTRON_BINARY_AND, alpha=0.01, w_range=0.2)
+    # run_one(**SimulationType.PERCEPTRON_BINARY_OR, alpha=0.01, w_range=0.2)
     run_one(**SimulationType.ADALINE_AND, alpha=0.01, w_range=0.2, max_err=0.3)
-    run_one(**SimulationType.ADALINE_OR, alpha=0.01, w_range=0.2, max_err=0.3)
+    # run_one(**SimulationType.ADALINE_OR, alpha=0.01, w_range=0.2, max_err=0.3)
 
     # n=25
     # alphas=[0.001, 0.003, 0.01, 0.03]
@@ -57,6 +61,7 @@ if __name__== "__main__":
     # run_many(params=alphas, n=n, simulation=alphas_simulation, **SimulationType.PERCEPTRON_BINARY_AND, alphas=alphas,
     #          w_range=w_range)
     #
+    # n = 25
     # w_ranges=[0.2, 0.4, 0.6, 0.8, 1]
     # alpha=0.01
     # run_many(params=w_ranges, n=n, simulation=weight_ranges_simulation, **SimulationType.ADALINE_OR,
