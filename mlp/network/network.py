adda92d2a9286dfdc8b44817ff602d6a3a899cfe
dominik <dominikheinisch2@gmail.com>
2019-10-20 16:22:16 +0200

    extracted func mlp

--- mlp/network/network.py
+++ mlp/network/network.py
@@ -20,7 +20,7 @@ def calc_prediction_accuracy(hidden_weight, output_weight, test_input, test_outp
     return np.sum(output_res == res) / output_res.shape[0]
 
 
-if __name__ == "__main__":
+def mlp():
     tr_zip, va_zip, te_zip = load_data_wrapper("../data")
     tr_in, tr_out = tr_zip
 
@@ -39,50 +39,34 @@ if __name__ == "__main__":
 
     weights.append([weights1, weights2])
     print('result: ', calc_prediction_accuracy(weights1, weights2, *te_zip))
-    # print(weights1.shape)
-    # print(tr_in.shape)
-    # print(tr_out.shape)
 
     for j in range(1):
-        for i in range(10000):
+        for i in range(images_len):
             net_hidden = tr_in[i] @ weights1
             hidden = activation_func(net_hidden)
             hidden_with_bias = np.ones(shape=(hidden.shape[0] + HIDDEN_BIAS))
             hidden_with_bias[HIDDEN_BIAS:] = hidden
-            # print(hidden)
 
             net_output = hidden_with_bias @ weights2
             output = activation_func(net_output)
-            # print(output.shape)
-            # print('output', output, tr_out[i])
 
-            # print('--------------------------------')
-            # print(output - tr_out[i])
-            # print('--------------------------------activation_func_prim')
-            # print(activation_func_prim(net_output))
-            # print('--------------------------------')
             err_out = (tr_out[i] - output) * activation_func_prim(net_output)
-            # print('temp:')
             temp = weights2[HIDDEN_BIAS:] @ err_out
-            # print(temp)
-            # print(activation_func_prim(net_hidden))
             err_hidden = temp * activation_func_prim(net_hidden)
-            # print('weights2', weights2.shape)
-            # print('err_out', err_out.shape)
-            # print('hidden', hidden.shape)
 
             weights2_delta = np.transpose(np.tile(hidden_with_bias, reps=(err_out.shape[0], 1))) * (alpha * err_out)
             weights2 = weights2 + weights2_delta
 
-            weights1_delta = np.transpose(np.tile(tr_in[i] * alpha, reps=(err_hidden.shape[0], 1))) * (alpha * err_hidden)
+            weights1_delta = np.transpose(np.tile(tr_in[i] * alpha, reps=(err_hidden.shape[0], 1))) * (
+                        alpha * err_hidden)
             weights1 = weights1 + weights1_delta
 
-            # print('delta2', weights2_delta.shape)
-            # print(weights2_delta)
-            # print(weights2)
-            # print(weights1)
             if (i + 1) % 1000 == 0:
-                print(i)
+                print(f'progress print: {i}')
         print(j, ' result: ', calc_prediction_accuracy(weights1, weights2, *te_zip))
         weights.append([weights1, weights2])
-    save(data=weights, filename=f'test_weights_8_bias_{HIDDEN_BIAS}.pkl')
+    return weights
+
+
+if __name__ == "__main__":
+    save(data=mlp(), filename=f'test_weights_8_bias_{HIDDEN_BIAS}.pkl')
