a0f76d56f68e313c4bf7bed964f5b4cc785148f0
dominik <dominikheinisch2@gmail.com>
2019-10-24 08:48:37 +0200

    main changes

--- mlp/prediction/main.py
+++ mlp/prediction/main.py
@@ -1,26 +1,16 @@
 from loader.loader import load
 from loader.mnist_loader import load_data_wrapper
 from prediction.network import calc_prediction_accuracy
-
-
-from contextlib import contextmanager
-from timeit import default_timer
-
-@contextmanager
-def elapsed_timer():
-    start = default_timer()
-    elapser = lambda: default_timer() - start
-    yield lambda: elapser()
-    end = default_timer()
-    elapser = lambda: end-start
-
+from utils.timer import elapsed_timer
 
 def print_result(filename, test_data):
-    te_in, te_out = test_data
-    weights = load(filename=filename)
-    print(filename)
-    for i in range(len(weights)):
-        print(f'{i}   {calc_prediction_accuracy(*weights[i], te_in, te_out)}')
+    with elapsed_timer() as timer:
+        te_in, te_out = test_data
+        weights = load(filename=filename)
+        print(filename)
+        for i in range(len(weights)):
+            print(f'{i}   {calc_prediction_accuracy(*weights[i], te_in, te_out)}')
+            # print(f'timer: {timer():.2f}')
 
 
 if __name__ == "__main__":
@@ -28,5 +18,14 @@ if __name__ == "__main__":
     # print_result('test_weights.pkl', test_data)
     # print_result('test_weights_12_bias_1.pkl', test_data)
     # print_result('test_weights_12_bias_1.pkl', validation_data)
-    print_result('test_weights_12_bias_1.pkl', test_data)
-    print_result('test_weights_18_bias_1_batch_1.pkl', test_data)
+
+    # print_result('test_weights_12_bias_1.pkl', test_data)
+    # print_result('test_weights_18_bias_1_batch_1.pkl', test_data)
+
+    # print_result('test_weights_20_bias_1_batch_1_momentum_0.3.pkl', test_data)
+    # print_result('test_weights_20_bias_1_batch_1_momentum_0.3.pkl', validation_data)
+
+
+    # print_result('test_weights_22_alpha_0.007_bias_1_batch_25_momentum_0.25_res_0.9644.pkl', test_data)
+    print_result('test_weights_22_alpha_0.007_bias_1_batch_25_momentum_0.25_res_0.9644.pkl', validation_data)
+
