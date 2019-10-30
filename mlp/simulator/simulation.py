e774f3e5679d453e3de46af36135c1f867ccea2a
dominik <dominikheinisch2@gmail.com>
2019-10-30 17:44:49 +0100

    aligned run_many to current mlp

--- mlp/simulator/simulation.py
+++ mlp/simulator/simulation.py
@@ -1,56 +1,54 @@
 import numpy as np
 
-from prediction.network import mlp_batch
+from prediction.network import mlp
 from loader.mnist_loader import load_data_wrapper
-from loader.loader import load
 from saver.saver import save
 
 
-def get_simul_params(alpha, batch_size, draw_range, epochs, momentum_param):
+def get_simul_params(alpha, batch_size, draw_range, worse_result_limit, momentum_param):
     return {
         'alpha': alpha,
         'batch_size': batch_size,
         'draw_range': draw_range,
-        'epochs': epochs,
+        'worse_result_limit': worse_result_limit,
         'momentum_param': momentum_param,
     }
 
 
 def run_once(func, params, **kwargs):
     results = func(**params, **kwargs)
-    to_save = {'result': results, **params}
-    # save(data=to_save, filename=f'test_weights_22_alpha_{alpha}_batch_{batch_size}_draw_{draw_range}'
-    #                             f'momentum_{momentum_param}_epochs_{epochs}.pkl')
-    print(to_save)
+    return {**results, **params}
 
 
 
 def run_many(times, func, params, **kwargs):
-    acc = []
+    many_results = []
     for t in range(times):
-        acc.append(func(**params, **kwargs)['accuracies'])
+        single_result = func(**params, **kwargs)
+        del single_result['weights']
+        many_results.append(single_result)
         print('simulation iter:', t)
-    print(acc)
-    avg_accuracy = np.asarray(acc).sum(axis=0) / len(acc)
-    print(avg_accuracy)
-    return {'times': times, 'avg_accuracy': avg_accuracy, **params}
+    print(many_results)
+    avg_epochs = sum([res['epochs'] for res in many_results]) / len(many_results)
+    return {'times': times, 'avg_epochs': avg_epochs, 'many_results': many_results, **params}
 
 
 if __name__ == "__main__":
-    loaded_data = load_data_wrapper("../data")
-    # params = get_simul_params(alpha=0.01, batch_size=25, draw_range=0.2, epochs=20, momentum_param=0)
-    params = get_simul_params(alpha=0.01, batch_size=50, draw_range=0.6, epochs=10, momentum_param=0)
     # # alpha = 0.015 # for batch=1
     # # momentum_param = 0.25
-    alpha, batch_size, draw_range, epochs, momentum_param = [params[p] for p in params]
-    times = 3
-    images_len_divider = 1
-    many_res = run_many(times=times, func=mlp_batch, data=loaded_data, params=params,
-                        images_len_divider=images_len_divider)
-    save(data=many_res, filename=f'simul_alpha_{alpha}_batch_{batch_size}_draw_{draw_range}_momentum_{momentum_param}_'
-                                 f'epochs_{epochs}_times_{times}.pkl')
-    print(many_res)
-    # print(load(filename='simul_alpha_0.007_batch_25_draw_0.2_momentum_0_epochs_2_times_3.pkl'))
-    # print(load(filename='simul_alpha_0.01_batch_25_draw_0.2_momentum_0_epochs_20_times_10.pkl'))
+    loaded_data = load_data_wrapper("../data")
+    # params = get_simul_params(alpha=0.01, batch_size=25, draw_range=0.2, epochs=20, momentum_param=0)
+    params = get_simul_params(alpha=0.02, batch_size=100, draw_range=0.2, worse_result_limit=2, momentum_param=0)
+    alpha, batch_size, draw_range, worse_result_limit, momentum_param = [params[p] for p in params]
 
-    # print(run_once(func=mlp_batch, data=loaded_data, params=params, images_len_divider=100))
+    # single_res = run_once(func=mlp, data=loaded_data, params=params, images_len_divider=50)
+    # print(single_res)
+    # save(data=mlp, filename=f'dummy_alpha_{alpha}_batch_{batch_size}_draw_range_{draw_range}_'
+    #                         f'epochs_{single_res["epochs"]}_res_{single_res["accuracies"][-1 - worse_result_limit]}_'
+    #                         f'momentum_param_{momentum_param}.pkl')
+
+    times = 5
+    many_res = run_many(times=times, func=mlp, data=loaded_data, params=params, images_len_divider=1)
+    print(many_res)
+    save(data=many_res, filename=f'alpha_simulation_alpha_{alpha}_batch_{batch_size}_draw_range_{draw_range}_'
+                                 f'avg_epochs_{many_res["avg_epochs"]}_times_{times}.pkl')
